8edcca067518aac93946b2fa81b8123bb10f6d67
sinanovicanes <sinanovicanes01@gmail.com>
2024-08-25 16:08:54 +0300

    refactor: Set global prefix for API Gateway and use global validation pipe

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ApiGatewayModule } from './api-gateway.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
   const configService = app.get(ConfigService);
   const PORT = configService.get<number>('PORT');
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(PORT);
 }
