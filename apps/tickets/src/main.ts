a1bd68737059c24f514c25c30649e800cb216022
sinanovicanes <sinanovicanes01@gmail.com>
2024-08-25 15:06:28 +0300

    feat: Update Tickets microservice setup with NATS integration

--- apps/tickets/src/main.ts
+++ apps/tickets/src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { TicketsModule } from './tickets.module';
+import { NatsFactory, NatsServices } from '@app/common/nats';
 
 async function bootstrap() {
-  const app = await NestFactory.create(TicketsModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(
+    TicketsModule,
+    NatsFactory.createOptions(NatsServices.TICKETS),
+  );
+  await app.listen();
 }
 bootstrap();
