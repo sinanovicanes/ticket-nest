7cdfdd9989736d758c640ae1d33f9653744bad51
sinanovicanes <sinanovicanes01@gmail.com>
2024-08-26 12:22:11 +0300

    feat: Add global validation pipe to Events microservice

--- apps/events/src/main.ts
+++ apps/events/src/main.ts
@@ -1,11 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { EventsModule } from './events.module';
 import { NatsFactory, NatsServices } from '@app/common/nats';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(
     EventsModule,
     NatsFactory.createOptions(NatsServices.EVENTS),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+    }),
   );
   await app.listen();
 }
