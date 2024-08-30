d5ab3521a8028996b3725a0dc657d80f9de7e49e
sinanovicanes <sinanovicanes01@gmail.com>
2024-08-30 10:53:02 +0300

    fix: Add stripe controller to the stripe module

--- apps/payments/src/stripe/stripe.module.ts
+++ apps/payments/src/stripe/stripe.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { StripeService } from './stripe.service';
+import { StripeController } from './stripe.controller';
 
 @Module({
   imports: [],
+  controllers: [StripeController],
   providers: [StripeService],
   exports: [StripeService],
 })
