c244fc753fa4c6ebf4d9993a44b09104b62939a4
sinanovicanes <sinanovicanes01@gmail.com>
2024-09-05 23:06:47 +0300

    fix: Add database module to locations microservice imports

--- apps/locations/src/locations.module.ts
+++ apps/locations/src/locations.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { LocationsController } from './locations.controller';
 import { LocationsService } from './locations.service';
+import { DatabaseModule } from '@app/database';
 
 @Module({
-  imports: [],
+  imports: [DatabaseModule],
   controllers: [LocationsController],
   providers: [LocationsService],
 })
